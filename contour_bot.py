3615ffcb1bc76e2f8371e41ff452f7349d81bc7f
roctbb <roctbb@gmail.com>
2021-09-15 18:40:52 +0300

    Initial

--- contour_bot.py
+++ contour_bot.py
@@ -37,7 +37,7 @@ def init(data):
     else:
         contract.active = True
 
-    medsenger_api.send_message(contract.id,
+    medsenger_api.send_message(data.get('contract_id'),
                                'К каналу консультирования подключен интеллектуальный агент для глюкометров Contour. Просто отправьте в чат CSV файл из мобильного приложения (через меню "поделиться"), и мы автоматически положим все измерения в вашу медицинскую карту.',
                                only_patient=True)
 
